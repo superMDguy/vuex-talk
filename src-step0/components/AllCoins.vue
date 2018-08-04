3800e7846804ea782212df1432bd242b1e6c44f3
Matthew Dangerfield <--global>
2018-08-04 17:18:08 -0500

    fix initial step

--- src-step0/components/AllCoins.vue
+++ src-step0/components/AllCoins.vue
@@ -3,41 +3,9 @@
     <h1 class="coinListTitle">Coins</h1>
 
     <ul class="coinList">
-      <li v-for="coin in coins" :key="coin.id">
-        <span>
-          {{ coin.name }}
-        </span>
-
-        <span>
-          {{ coin.quotes.USD.price | currency }}
-        </span>
-
-        <div class="actions">
-          <font-awesome-icon v-if="inPortfolio(coin)" size="lg" icon="check" class="action added" />
-          <font-awesome-icon v-else size="lg" icon="cart-plus" @click="addToPortfolio(coin)" title="Add to portfolio" class="clickable action add" />
-        </div>
-      </li>
     </ul>
   </div>
 </template>
-
-<script>
-import { mapActions, mapGetters, mapState } from 'vuex'
-
-export default {
-  computed: {
-    ...mapState(['coins']),
-    ...mapGetters(['inPortfolio'])
-  },
-  methods: {
-    ...mapActions(['fetchCoins', 'addToPortfolio'])
-  },
-  created() {
-    this.fetchCoins()
-  }
-}
-</script>
-
 
 <style lang="scss" scoped>
 @import '../styles/coinList';
--- src-step0/main.js
+++ src-step0/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 import axios from 'axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
@@ -29,6 +28,5 @@ Vue.config.productionTip = false
 axios.defaults.baseURL = '/api'
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
