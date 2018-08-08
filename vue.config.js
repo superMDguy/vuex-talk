6c551ea9be8cb630704588ba5423105b3a2bbf9d
Matthew Dangerfield <--global>
2018-08-08 13:51:28 -0500

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,6 @@ const srcFolder = `${__dirname}/src-${process.env.STEP}`
 module.exports = {
   lintOnSave: false,
 
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://api.coinmarketcap.com/v2/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  },
-
   configureWebpack: {
     resolve: {
       alias: {
